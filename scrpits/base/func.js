b106540f9853490bec8c0e9a10562fd7201b7c26
wwweio <wwweio@outlook.com>
2024-04-09 23:23:18 +0800

    scripts：func

---
+++ scrpits/base/func.js
@@ -0,0 +1,35 @@
+// 函数声明
+function functionName(parameters) {
+    // 函数体
+  }
+  
+  // 匿名函数赋值给变量
+  var functionName = function(parameters) {
+    // 函数体
+  };
+  
+  // 箭头函数
+  var functionName = (parameters) => {
+    // 函数体
+  };
+  
+  // ES6 方法定义
+  const object = {
+    functionName(parameters) {
+      // 函数体
+    }
+  };
+  
+  // 构造函数
+  function ConstructorFunction(parameters) {
+    this.property = parameters;
+    this.method = function() {
+      // 方法体
+    };
+  }
+  
+  // 原型方法
+  ConstructorFunction.prototype.methodName = function(parameters) {
+    // 方法体
+  };
+  
