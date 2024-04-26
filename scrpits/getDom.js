e39ba1cfbe78f4bc95aa97427929808182f0b0f3
wwweio <wwweio@outlook.com>
2024-04-26 23:41:48 +0800

    scripts：get dom

---
+++ scrpits/getDom.js
@@ -0,0 +1,14 @@
+// 通过 ID 获取元素
+let elementById = document.getElementById('elementId');
+
+// 通过类名获取元素（返回一个节点列表）
+let elementsByClassName = document.getElementsByClassName('className');
+
+// 通过标签名获取元素（返回一个节点列表）
+let elementsByTagName = document.getElementsByTagName('tagName');
+
+// 通过选择器获取单个元素
+let elementBySelector = document.querySelector('selector');
+
+// 通过选择器获取多个元素（返回一个节点列表）
+let elementsBySelectorAll = document.querySelectorAll('selector');
