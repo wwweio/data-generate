3f6343577e2434b10ca3d57086bc99c1735fd863
wwweio <wwweio@outlook.com>
2024-04-28 00:05:43 +0800

    mofidy the  scanDirs to compatible with defferent systems

--- scripts/scanDirs.js
+++ scripts/scanDirs.js
@@ -1,14 +1,12 @@
 import fs from 'fs';
 import path from 'path';
 
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
-const directoryPath = path.join(__dirname, '../practice');
-const outputPath = path.join(__dirname, '../public', 'directories.json');
+// 使用 process.cwd() 获取当前工作目录，然后根据需要调整路径
+const directoryPath = path.join(process.cwd(), 'practice');
+const outputPath = path.join(process.cwd(), 'public', 'directories.json');
 
 function getDirectories(dirPath) {
     const files = fs.readdirSync(dirPath, { withFileTypes: true });
-    // const directories = files.filter(file => file.isDirectory()).map(dir => dir.name);
-    // return directories;
     const directories = files.filter(file => file.isDirectory()).map(dir => {
         const num = parseInt(dir.name.match(/\d+/)[0], 10); // 提取目录名称中的数字部分并转换为数字
         return { name: dir.name, num };
@@ -17,8 +15,6 @@ function getDirectories(dirPath) {
     return sortedDirectories.map(dir => dir.name);
 }
 
-// const allDirectories = getDirectories(directoryPath);
-// fs.writeFileSync(outputPath, JSON.stringify(allDirectories), 'utf-8');
 const allDirectories = getDirectories(directoryPath);
 const formattedDirectories = JSON.stringify(allDirectories, null, 2); // 格式化JSON，缩进为2个空格
 
